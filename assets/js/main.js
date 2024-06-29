b54b881b4e05f8d8d76267939a6d7d135d5fcaee
Eby Jimmy <ebyjimmy1995@gmail.com>
2024-06-29 14:04:54 +0530

    initial commit for portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,30 +79,25 @@ tabs.forEach(tab =>{
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-/*const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrY = window.scrollY;
+const scrollActive = () =>{
+    const scrollDown = window.scrollY;
 
     sections.forEach(current =>{
-        const sectionHeight = current.scrollHeight;
-        const sectionTop = current.scrollTop - 50;
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
         const sectionId = current.getAttribute('id');
-
-        const link = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
-        if(link){
-            if(scrY > sectionTop && scrY <= sectionTop + sectionHeight){
-                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-            }else{
-                document.querySelector('.nav_menu a[hreft*=' + sectionId + ']').classList.remove('active-link')
-            }
+        sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')        
+       
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
         }else{
-            console.warn('No link found for section ID: ', sectionId);
-        }
-        
+        sectionsClass.classList.remove('active-link')
+        }      
     })
 }
-window.addEventListener('scroll', scrollActive)*/
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
@@ -139,4 +134,98 @@ themeButton.addEventListener('click', () =>{
 
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*==================== SEND EMAIL ====================*/ 
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Subject: ${subject.value}<br> Message: ${mess.value}`
+
+    Email.send({
+        SecureToken : "473d3126-9619-4dcd-bc77-2a195495ad74",
+        To : 'ebyjimmy1995@gmail.com',
+        From : "ebyjimmy1995@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if(message == "OK") {
+            Swal.fire({
+                title: "Success",
+                text: "Message sent successfully!",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+
+function checkInputs() {
+    const items = document.querySelectorAll(".contact__input");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        })
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            } else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error__text.email");
+
+    if (!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter a valid email address";
+        } else {
+            errorTxtEmail.innerText = "Email id can't be blank";
+        }
+
+    } else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if(!fullName.classList.contains("error") && !email.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+    
 })
